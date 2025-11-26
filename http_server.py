930ffda35d74165ab51d1123ae62cf9e6f751a6d
mohakgoel-data <mohakgoel.data@gmail.com>
2025-11-26 15:09:22 +0530

    parsing function working

--- http_server.py
+++ http_server.py
@@ -1,18 +1,78 @@
 import socket
+import urllib.parse
+
+class Request:
+    def __init__(self):
+        self.method = None
+        self.path = None
+        self.version = None
+        self.headers={}
+        self.query_params={}
+        self.body=None
+        self.id=None
+
+def read_full_request(client_socket):
+    request_data=b""
+    header_end=b"\r\n\r\n"
+
+    while header_end not in request_data:
+        chunk = client_socket.recv(1024)
+        if not chunk:
+            return b""
+        request_data+=chunk
+    
+    return request_data
+
+def parse_request(data):
+    request=Request()
+
+    try:
+        request_str=data.decode("utf-8")
+        lines = request_str.split('\r\n')
+
+        request_line = lines[0]
+        parts = request_line.split(' ')
+        if len(parts) != 3:
+            raise ValueError("Malformed request line")
+        request.method, full_path, request.version = parts
+        url_parts = urllib.parse.urlparse(full_path)
+        request.path = url_parts.path
+        request.query_params=urllib.parse.parse_qs(url_parts.query)
+
+        for line in lines[1:]:
+            if not line:
+                break
+
+            key, value=line.split(":",1)
+            request.headers[key.strip().lower()]=value.strip()
+
+        return request
+
+
+    except Exception as e:
+        print("error occured: ",e)
+        return None
+
+    finally:
+        print("Parsing completed")
 
 def handle_client_connection(client_socket,client_address):
     print(f"Accepted connection from {client_address}")
 
     try:
-        request_data=client_socket.recv(4096)
+        request_data=read_full_request(client_socket)
         if not request_data:
             return
-        print("\n--- Raw Request Data ---")
-        print(request_data.decode('utf-8', errors='ignore'))
-        print("------------------------\n")
+        request=parse_request(request_data)
+
+        print(f"Method: {request.method}")
+        print(f"Path: {request.path}")
+        print(f"Headers (Host): {request.headers.get('host', 'N/A')}")
+        print(f"Query Params: {request.query_params}")
 
         dummy_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 20\r\nConnection: close\r\n\r\nServer is responding."
         client_socket.sendall(dummy_response.encode('utf-8'))
+
 
     except Exception as e:
         print(f"Error handling client {client_address}: {e}")
